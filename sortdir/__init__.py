198ac9b258137f86eddb5780ad42838bb617be94
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-06-30 17:06:46 +0300

    Read data from a config file

--- sortdir/__init__.py
+++ sortdir/__init__.py
@@ -6,7 +6,7 @@ from watchdog.events import PatternMatchingEventHandler
 
 def move_file(filepath, extension_to_directory, path):
     _, ext = os.path.splitext(filepath)
-    directory_name = extension_to_directory[ext]
+    directory_name = extension_to_directory[ext.lower()]
     directory_path = os.path.join(path, directory_name)
 
     if not os.path.isdir(directory_path):
@@ -20,7 +20,7 @@ def perform_initial_sorting(extension_to_directory, path):
         _, ext = os.path.splitext(filename)
         filepath = os.path.join(path, filename)
 
-        if os.path.isfile(filepath) and ext in extension_to_directory:
+        if os.path.isfile(filepath) and ext.lower() in extension_to_directory:
             move_file(filepath, extension_to_directory, path)
 
 
--- sortdir/__main__.py
+++ sortdir/__main__.py
@@ -1,11 +1,13 @@
+import os
 import logging
-import sys
 import time
 
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 
 from sortdir import perform_initial_sorting, schedule_sorting_handler
+from sortdir.config import config as parse_config
+from sortdir.utils import print_error
 
 
 logging.basicConfig(
@@ -13,44 +15,48 @@ logging.basicConfig(
     format="%(asctime)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
-path = sys.argv[1] if len(sys.argv) > 1 else "."
-
-directory_to_extensions = {
-    "documents": {
-        ".doc",
-        ".docx",
-        ".ods",
-        ".odt",
-        ".pdf",
-        ".ppt",
-        ".pptx",
-        ".txt",
-        ".xls",
-        ".xlsx",
-    },
-    "images": {".gif", ".png", ".jpeg", ".jpg"},
-}
-extension_to_directory = {
-    pat: ext
-    for ext, patterns in directory_to_extensions.items()
-    for pat in patterns
-}
-
-# sort directory
-perform_initial_sorting(extension_to_directory, path)
 
 # set up observer
 observer = Observer()
 
 logging_event_handler = LoggingEventHandler()
-observer.schedule(logging_event_handler, path, recursive=False)
-schedule_sorting_handler(observer, extension_to_directory, path)
+
+config = parse_config()
+directories = config["directories"]
+
+directories_to_watch = []
+for path in directories:
+    if not os.path.isdir(path):
+        print_error(f"Directory '{path}' does not exist")
+        continue
+
+    directories_to_watch.append(path)
+
+    directory_to_extensions = directories[path]
+    extension_to_directory = {
+        pat: ext
+        for ext, patterns in directory_to_extensions.items()
+        for pat in patterns
+    }
+
+    # sort directory
+    perform_initial_sorting(extension_to_directory, path)
+
+    observer.schedule(logging_event_handler, path, recursive=False)
+    schedule_sorting_handler(observer, extension_to_directory, path)
 
 # wait for events
-observer.start()
-try:
-    while True:
-        time.sleep(1)
-finally:
-    observer.stop()
-    observer.join()
+if directories_to_watch:
+    stringified_directories = ",\n".join(directories_to_watch)
+    print(
+        f"Watching for changes in following directories:\n{stringified_directories}",
+        end="\n\n",
+    )
+
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
